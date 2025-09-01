9cfec7644cd0bb9fd95e8d86e2fd2be31c5588d6
Do Duy Dong <duydong.do.sdet@gmail.com>
2025-09-01 11:46:59 +0700

    Relative Locators

--- selenium/webdriver/S_02_Selenium_Locators.java
+++ selenium/webdriver/S_02_Selenium_Locators.java
@@ -3,6 +3,7 @@ package webdriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -21,45 +22,35 @@ public class S_02_Selenium_Locators {
     }
 
     @Test
-    public void TC_01_By_Id() {
+    public void TC_01_Locators_By() {
         driver.findElement(By.id("email"));
-    }
 
-    @Test
-    public void TC_02_By_Class() {
         driver.findElement(By.className("fb_logo"));
-    }
 
-    @Test
-    public void TC_03_By_Name() {
         driver.findElement(By.name("login"));
-    }
 
-    @Test
-    public void TC_04_By_Tag_Name() {
         driver.findElement(By.tagName("input"));
-    }
 
-    @Test
-    public void TC_05_By_Link_Text() {
         driver.findElement(By.linkText("Tiếng Việt"));
-    }
 
-    @Test
-    public void TC_06_By_Partial_Link_Text() {
         driver.findElement(By.partialLinkText("ếng Việ"));
-    }
 
-    @Test
-    public void TC_07_By_XPath() {
         driver.findElement(By.xpath("//input[@id='email']"));
-    }
 
-    @Test
-    public void TC_08_By_CSS() {
         driver.findElement(By.cssSelector("input[id='email']"));
     }
 
+    @Test
+    public void TC_02_Relative_Locators() {
+        By englishBy = By.xpath("//li[text()='English (UK)']");
+        By signUpBy = By.xpath("//a[text()='Sign Up']");
+        By messengerBy = By.xpath("//a[text()='Messenger']");
+        By threadsBy = By.xpath("//a[text()='Threads']");
+        By helpBy = By.xpath("//a[text()='Help']");
+
+        By elementXPath = RelativeLocator.with(By.xpath("//a")).above(threadsBy).below(englishBy).toLeftOf(messengerBy).toRightOf(signUpBy).near(helpBy);
+        System.out.println(driver.findElement(elementXPath).getText());
+    }
 
     @AfterClass
     public void afterClass() {

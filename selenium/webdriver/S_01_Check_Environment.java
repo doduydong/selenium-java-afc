bff23bcb48a5b284da7a85d1cf0ce020a0b76a15
Do Duy Dong <doduydong1310@gmail.com>
2026-01-03 14:00:09 +0700

    Initial commit

---
+++ selenium/webdriver/S_01_Check_Environment.java
@@ -0,0 +1,43 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class S_01_Check_Environment {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+    }
+
+    @Test
+    public void TC_01_Url() {
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
+    }
+
+    @Test
+    public void TC_02_Logo() {
+        Assert.assertTrue(driver.findElement(By.cssSelector("img.fb_logo")).isDisplayed());
+    }
+
+    @Test
+    public void TC_03_Form() {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}

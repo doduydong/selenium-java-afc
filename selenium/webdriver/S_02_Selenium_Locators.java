8481a6e9f9c113c92704f8969dadb7e1b629e606
Do Duy Dong <doduydong1310@gmail.com>
2026-01-19 15:37:54 +0700

    Relative Locators

--- selenium/webdriver/S_02_Selenium_Locators.java
+++ selenium/webdriver/S_02_Selenium_Locators.java
@@ -2,7 +2,9 @@ package webdriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -58,6 +60,17 @@ public class S_02_Selenium_Locators {
     @Test
     public void TC_08_By_CSS() {
         driver.findElement(By.cssSelector("input[id='email']"));
+    }
+
+    @Test
+    public void TC_09_Relative_Locators() {
+        By tiengVietBy = By.xpath("//a[text()='Tiếng Việt']");
+        By privacyPolicyBy = By.xpath("//a[text()='Privacy Policy']");
+        By logInBy = By.xpath("//a[text()='Log in']");
+        By fbLite = By.xpath("//a[text()='Facebook Lite']");
+
+        WebElement messenger = driver.findElement(RelativeLocator.with(By.xpath("//a")).above(privacyPolicyBy).below(tiengVietBy).toLeftOf(fbLite).toRightOf(logInBy));
+        System.out.println(messenger.getText());
     }
 
     @AfterClass
